a4097c0f5566128b50dcdba2339f19c5b5df1819
rsang <richardsang2008@gmail.com>
2016-08-04 15:02:27 -0700

    init

---
+++ test/test1.py
@@ -0,0 +1,25 @@
+
+import logging
+
+from src.utils.NetworkHelper import NetworkHelper
+
+
+def main():
+    with open("app.log", "w") as file:
+        file.truncate()
+    logging.basicConfig(filename="app.log",
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        level= logging.DEBUG,
+                        mode ="w")
+
+
+    logging.info("start this")
+    networkhelper = NetworkHelper(logging)
+    urlStr = "http://travel.state.gov/content/visas/english/law-and-policy/bulletin.html"
+    networkhelper.visa_bulletin_geturls(urlStr)
+    for key, value in networkhelper.visaboardMaps.iteritems():
+        logging.debug(key.strftime("%A %d. %B %Y")+"=====>"+ value)
+    logging.info("end")
+
+if __name__=="__main__" :
+    main()
